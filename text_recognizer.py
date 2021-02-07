ef1eed570c8fd607deb94176ce8cea7128d27608
lidongliang666 <1069659379@qq.com>
2021-02-07 11:55:52 +0800

    feat:文本识别增加predict(imgpath,boxes_list)方法

--- text_recognizer.py
+++ text_recognizer.py
@@ -7,6 +7,9 @@ print(str(__dir__.parent))
 sys.path.insert(3,str(__dir__.parent))
 
 import torch
+import cv2
+from PIL import Image
+import numpy as np
 
 from  model import Model
 from recognizer_utils import CTCLabelConverter
@@ -64,6 +67,61 @@ class OcrTextRecognizer:
 
         return result_list
 
+    def predict(self,imgpath,boxes_list):
+        img = cv2.imread(imgpath,flags=cv2.IMREAD_GRAYSCALE)
+        pilimglist = self.get_pilimglist(img,boxes_list)
+        txtlist = self.__call__(pilimglist)
+        return txtlist
+
+    def get_pilimglist(self,imgcopy,bboxlist):
+        #过滤那些 竖着的框（高比宽大）
+        pilimglist = []
+        for _,bbox in enumerate(bboxlist):
+            imgcut = self.get_rotate_crop_image(imgcopy,bbox)
+            if imgcut is None:
+                continue
+            #图片裁剪物可视化
+            # cv2.imwrite(f"{i}.jpg",imgcut)
+            pilimglist.append(imgcut)
+        return pilimglist
+
+    def get_rotate_crop_image(self, img, points):
+        '''
+        img_height, img_width = img.shape[0:2]
+        left = int(np.min(points[:, 0]))
+        right = int(np.max(points[:, 0]))
+        top = int(np.min(points[:, 1]))
+        bottom = int(np.max(points[:, 1]))
+        img_crop = img[top:bottom, left:right, :].copy()
+        points[:, 0] = points[:, 0] - left
+        points[:, 1] = points[:, 1] - top
+        '''
+        points = np.array(points,dtype="float32")
+        img_crop_width = int(
+            max(
+                np.linalg.norm(points[0] - points[1]),
+                np.linalg.norm(points[2] - points[3])))
+        img_crop_height = int(
+            max(
+                np.linalg.norm(points[0] - points[3]),
+                np.linalg.norm(points[1] - points[2])))
+        if img_crop_height > img_crop_width:
+            return None
+        pts_std = np.float32([[0, 0], [img_crop_width, 0],
+                              [img_crop_width, img_crop_height],
+                              [0, img_crop_height]],dtype="float32")
+        M = cv2.getPerspectiveTransform(points, pts_std)
+        dst_img = cv2.warpPerspective(
+            img,
+            M, (img_crop_width, img_crop_height),
+            borderMode=cv2.BORDER_REPLICATE,
+            flags=cv2.INTER_CUBIC)
+        # dst_img_height, dst_img_width = dst_img.shape[0:2]
+        # if dst_img_height * 1.0 / dst_img_width >= 1.5:
+        #     dst_img = np.rot90(dst_img)
+        pilimg = Image.fromarray(dst_img).convert("L")
+        return pilimg
+
 
 
 
@@ -81,18 +139,19 @@ if __name__ == "__main__":
     # a =OcrTextRecognizer("../TextRecognitionDataGenerator/trdg/mydicts/ppocr_keys_v1.txt")
     # print(a.text_recognizer)
     ################
-    from PIL import Image
-    imgdir = "/home/ldl/桌面/论文/文本识别/data/IIIT5K/test"
-    imglist = []
-    n = 0
-    for imgname in os.listdir(imgdir):
-        imgpath = os.path.join(imgdir,imgname)
-        print(imgpath)
-        imglist.append(Image.open(imgpath).convert('L'))
-        n += 1
-        if n == 10:
-            break
-    m = OcrTextRecognizer("../TextRecognitionDataGenerator/trdg/mydicts/ppocr_keys_v1.txt",device="cuda")
-    m.setweigth("/home/ldl/桌面/论文/文本识别/deep_text_recognition_benchmark/saved_models/None-RCNN-BiLSTM-CTC-Seed1111/best_accuracy.pth")
-    print(len(imglist))
-    print(m(imglist))
+    # from PIL import Image
+    # imgdir = "/home/ldl/桌面/论文/文本识别/data/IIIT5K/test"
+    # imglist = []
+    # n = 0
+    # for imgname in os.listdir(imgdir):
+    #     imgpath = os.path.join(imgdir,imgname)
+    #     print(imgpath)
+    #     imglist.append(Image.open(imgpath).convert('L'))
+    #     n += 1
+    #     if n == 10:
+    #         break
+    m = OcrTextRecognizer("../TextRecognitionDataGenerator/trdg/mydicts/ppocr_keys_v1.txt",
+        weigthpath="/home/ldl/桌面/论文/文本识别/deep_text_recognition_benchmark/saved_models/None-RCNN-BiLSTM-CTC-Seed1111/best_accuracy_recognizer.pth"
+        ,device="cuda")
+    # model_cp = "/home/ldl/桌面/论文/文本识别/deep_text_recognition_benchmark/saved_models/None-RCNN-BiLSTM-CTC-Seed1111/best_accuracy_recognizer.pth"
+    # torch.save(m.text_recognizer.state_dict(), model_cp,_use_new_zipfile_serialization=False)
